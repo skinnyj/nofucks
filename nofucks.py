e4f18951ee849971945cc234e0c151b1558adee2
skinnyj <jarednathanielthomas@gmail.com>
2017-06-07 02:07:10 -0500

    Update nofucks.py
    included the possibility of giving a single fuck

--- nofucks.py
+++ nofucks.py
@@ -2,4 +2,7 @@ fucks = 2
 
 while fucks != 0:
     fucks = int(raw_input("How many fucks do you give? "))
-    print "I count a total of %s fucks" % fucks
+    if fucks == 1:
+        print "I count a total of %s fuck" % fucks
+    else:
+        print "I count a total of %s fucks" % fucks
