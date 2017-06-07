224c192b95d7e1c2d1b8573766652d9f8dd8bbd4
skinnyj <jarednathanielthomas@gmail.com>
2017-06-07 02:34:39 -0500

    Update nofucks.py
    more fucks
    I don't think I'm doing this right

--- nofucks.py
+++ nofucks.py
@@ -1,8 +1,17 @@
 fucks = 2
+null = ""
 
-while fucks != 0:
+while fucks != null:
     fucks = int(raw_input("How many fucks do you give? "))
-    if fucks == 1:
+
+    if fucks == 0:
+        print "Good for you!"
+        break
+    elif fucks > 2:
+        print "I count a total of %s fucks" % fucks
+        print "You give way too many fucks"
+    elif fucks < 2:
         print "I count a total of %s fuck" % fucks
+        print "You're so close"
     else:
         print "I count a total of %s fucks" % fucks
