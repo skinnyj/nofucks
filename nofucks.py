d95b83a1d230c8a74e97d284e74a20fe16ec8fad
skinnyj <jarednathanielthomas@gmail.com>
2017-06-08 00:51:13 -0500

    Update nofucks.py

--- nofucks.py
+++ nofucks.py
@@ -3,15 +3,25 @@ null = ""
 
 while fucks != null:
     fucks = int(raw_input("How many fucks do you give? "))
-
+  
     if fucks == 0:
-        print "Good for you!"
+        print "Good for you!\n"
         break
-    elif fucks > 2:
+
+    elif fucks > 5:
         print "I count a total of %s fucks" % fucks
         print "You give way too many fucks"
+        print "Try....giving less of a fuck\n"
+
+    elif fucks > 2:
+        print "I count a total of %s fucks" % fucks
+        print "That's still way too many"
+        print "Let's try this again...\n"
+
     elif fucks < 2:
         print "I count a total of %s fuck" % fucks
-        print "You're so close"
+        print "You're so close\n"
+
     else:
         print "I count a total of %s fucks" % fucks
+        print "A baseline level of fuckery\n"
